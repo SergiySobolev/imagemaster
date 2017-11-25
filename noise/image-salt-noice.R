9b3b7574635bff0b4b1e4075896ee2faf28cdf03
SergiySobolev <sergiy.sobolev@gmail.com>
2017-11-26 00:29:07 +0200

    grayscale

--- noise/image-salt-noice.R
+++ noise/image-salt-noice.R
@@ -1,9 +1,28 @@
 library(imager)
+library(raster)
+library(spatialfil)
+
+tograyscale <- function(i) {
+  grayscale(i)
+}
 
 imgPath <- "noise/i.jpg";
-file <- load.image(imgPath)
-plot(file)
+i <- load.image(imgPath)
+gi <- tograyscale(i)
+m <- as.matrix(gi)
+flt <- as.cimg(matrix(1,4,4)/16)
+fi <- convolve(gi,flt)
 
+
+  
+
+
+kernel <- convKernel(sigma = 1.4, k = 'gaussian')
+#fi = applyFilter(m, kernel);
+
+plot(i,main="Original")
+plot(gi, main="To Grayscale")
+plot(image(m), main="Blurred")
 
 
 
