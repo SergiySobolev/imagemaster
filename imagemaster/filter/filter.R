607596c82d652a64b5a12e9b1378c1aebd1022dc
SergiySobolev <sergiy.sobolev@gmail.com>
2017-11-28 23:04:11 +0200

    UnOptimized median filter procedure

--- imagemaster/filter/filter.R
+++ imagemaster/filter/filter.R
@@ -1,13 +1,31 @@
-MMmedianFilter <- function(img, kernelSize) {
-  kernelRadius <- (kernelSize + 1)/2
+MmedianFilter <- function(img, kernelSize) {
+  
   d <- dim(img)
-  fimg = matrix(0, d[1], d[2])
-  for (i in 2:d[1]-1) {
-    for(j in 2:d[2]-1) {
-      sumimg = img(i-1:i+1, j-1:j+1)
-      m = median(sumimg)
-      fimg[i,j] = m
+  imgcj <- CJ(1:nrow(img), 1:ncol(img))
+  mymedian <- function(v, m, k) {
+    i = v[1]
+    j = v[2]
+    k <- k/2
+    if (i>k && j>k && i<nrow(m)-k+1 && j<ncol(m)-k+1) {
+      subimg <- c (
+        m[i-1, j-1], m[i-1, j], m[i-1,j+1],
+        m[i,   j-1], m[i,   j], m[i,  j+1],
+        m[i+1, j-1], m[i+1, j], m[i+1,j+1]
+      )
+      # subimg <- vector()
+      # for(l in -k:k) {
+      #   for(u in -k:k) {
+      #     p <- subimg
+      #     subimg <- c(p, m[i+l,j+u])  
+      #   }
+      # }
+      r <- median(subimg)
+    } else {
+      r = m[i,j]
     }
+    return(r)
   }
-  return(fimg)
+  f <- apply(X = imgcj, MARGIN = 1, FUN = mymedian, m=img, k=kernelSize)
+  f <- matrix(f, nrow(img), ncol(img), byrow = TRUE);
+  return(f)
 }
--- noise/image-salt-noice.R
+++ noise/image-salt-noice.R
@@ -2,29 +2,9 @@ library(raster)
 library(jpeg)
 library(mmand)
 
-
-
 source(file = "imagemaster/grayscale.R")
-#source(file = "imagemaster/filter/filter.R")
+source(file = "imagemaster/filter/filter.R")
 source(file = "imagemaster/noice.R")
-
-MmedianFilter <- function(img, kernelSize) {
-  kr <- (kernelSize + 1)/2
-  d <- dim(img)
-  fimg <- matrix(0, d[1], d[2])
-  lx <- kr + 1
-  rx <- d[1] - kr
-  by <- kr + 1
-  uy <- d[2] - kr
-  for (i in lx:300) {
-    for(j in by:300) {
-      subimg <- img[i-kr:i+kr, j-kr:j+kr]
-      m <- median(subimg)
-      fimg[i,j] <- m
-    }
-  }
-  return(fimg)
-}
 
 plotImage <- function(img, title) {
   res = dim(img)[2:1]
@@ -45,15 +25,15 @@ plotImage(glimg, "Grayscale Image based on Lightness")
 pepperNoicedImg <- addPepperNoice(gimg)
 plotImage(pepperNoicedImg, "Paper noised image")
 
-#mImg <- MmedianFilter(pepperNoicedImg, 3)
-k <- shapeKernel(c(3,3), type="box")
-mImg <- mmand::medianFilter(pepperNoicedImg, k)
-plotImage(mImg, "Filtered with Median filter with size 3")
+k = 3
+t <- system.time(mImg <- MmedianFilter(pepperNoicedImg, k))
+s = paste("Filtered with Median filter with size=", k ,". Time spent=", format(round(t[1], 2), nsmall = 2))
+plotImage(mImg, s)
 
-mmImg <- mmand::medianFilter(mImg, k)
-plotImage(mmImg, "Second time Filtered with Median filter with size 3")
-mmmImg <- mmand::medianFilter(mImg, k)
-plotImage(mmmImg, "Third time Filtered with Median filter with size 3")
+kernel <- shapeKernel(c(k,k), type="box")
+t <- system.time(mmImg <- mmand::medianFilter(pepperNoicedImg, kernel))
+s = paste("Filtered with mmand:Median filter with size=", k ,". Time spent=", format(round(t[1], 2), nsmall = 2))
+plotImage(mmImg, s)
 
 
 
